3afd6a48ac42c3ba123c5af4db68ffb16f322411
unknown <johnwendellbalagot11111@gmail.com>
2020-07-31 01:41:38 +0800

    upd

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,14 +10,19 @@ const withErrorHandler = (WrappedComponent, axios) =>{
         }
         componentDidMount(){
 
-            axios.interceptors.request.use(req => {
+            this.resInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error:null});
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.reqInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () =>{
